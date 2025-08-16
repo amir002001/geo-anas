25b5a56a6d02b69d457fb450aa045b5f2b0bf388
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2025-08-16 01:14:28 -0400

    diff zoom levels and ittles for demo tomorrow

--- src/timelines/displacementTimeline/displacementTimeline.tsx
+++ src/timelines/displacementTimeline/displacementTimeline.tsx
@@ -1,7 +1,7 @@
 import type { Timeline } from "../timelines";
 
 export const displacementTimeline: Timeline = [
-  { flyTo: { zoom: 16 }, title: "foo", sources: [] },
-  { flyTo: { zoom: 16 }, title: "bar", sources: [] },
-  { flyTo: { zoom: 16 }, title: "baz", sources: [] },
+  { flyTo: { zoom: 16 }, title: "zoom 16", sources: [] },
+  { flyTo: { zoom: 16 }, title: "still zoom 16", sources: [] },
+  { flyTo: { zoom: 18 }, title: "zoom 18", sources: [] },
 ];
