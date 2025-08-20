d25dde98504c385f7bed4c68f406c0aca109e389
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2025-08-19 20:06:55 -0400

    base array

---
+++ src/timelines/preOctSeven/education.ts
@@ -0,0 +1,16 @@
+import type { GazaEvent } from "../timelines";
+
+export const universities: GazaEvent = {
+  fitBounds: {
+    bounds: [
+      [34.466, 31.5],
+      [34.6, 31.6],
+    ],
+    options: {},
+  },
+  layerOverrides: [],
+  title: "Education",
+  kind: "basic",
+};
+
+export const education = [universities];
--- src/timelines/preOctSeven/index.ts
+++ src/timelines/preOctSeven/index.ts
@@ -1,12 +1,28 @@
 import type { Timeline } from "../timelines";
+import { balfour } from "./balfour";
 import { crossings } from "./crossings";
+import { economy } from "./economy";
+import { education } from "./education";
 import { eighteenYearBlockade } from "./eighteenYearBlockade";
 import { ironFenceAroundStrip } from "./fenceAroundTheStrip";
+import { foodAccessAndAgriculture } from "./foodAccessAndAgriculture";
+import { healthcare } from "./healthcare";
 import { hiribya } from "./hiribya";
+import { powerAccess } from "./powerAccess";
+import { spatiality } from "./spatiality";
+import { water } from "./water";
 
 export const preOctSeven: Timeline = [
   ...eighteenYearBlockade,
   ironFenceAroundStrip,
+  ...balfour,
   ...hiribya,
   ...crossings,
+  ...spatiality,
+  ...foodAccessAndAgriculture,
+  ...water,
+  ...powerAccess,
+  ...healthcare,
+  ...economy,
+  ...education,
 ];
