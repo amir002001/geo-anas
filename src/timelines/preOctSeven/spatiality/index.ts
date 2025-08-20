9d4da8fc811737ffae522e1fc7ff3f1d0eaf9fc3
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2025-08-20 19:54:03 -0400

    remove everything but arrows

--- src/timelines/preOctSeven/spatiality/index.ts
+++ src/timelines/preOctSeven/spatiality/index.ts
@@ -1,3 +1,15 @@
+import { GAZA_BBOX } from "../../../constants/constants";
+import type { GazaEvent } from "../../timelines";
 import { refugeeCamps } from "./refugeeCamps";
 
-export const spatiality = [refugeeCamps];
+const population: GazaEvent = {
+  fitBounds: {
+    bounds: GAZA_BBOX,
+    options: {},
+  },
+  kind: "basic",
+  title: "Geographic Spatiality",
+  layerOverrides: [],
+};
+
+export const spatiality = [population, refugeeCamps];
