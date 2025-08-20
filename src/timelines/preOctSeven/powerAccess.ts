cfdb515c606cbfe5c3e9d518bb245da86b2a16dd
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2025-08-19 21:27:52 -0400

    power access

--- src/timelines/preOctSeven/powerAccess.ts
+++ src/timelines/preOctSeven/powerAccess.ts
@@ -1,1 +1,20 @@
-export const powerAccess = [];
+import type { LngLatBoundsLike } from "mapbox-gl";
+import type { GazaEvent } from "../timelines";
+
+const GAZA_PPLANT_BOUNDS: LngLatBoundsLike = [
+  [34.39867080760257, 31.451685942978582], // Southwest corner
+  [34.40544712745469, 31.458462518018624], // Northeast corner
+];
+
+const gazaPowerPlant: GazaEvent = {
+  fitBounds: {
+    bounds: GAZA_PPLANT_BOUNDS,
+    options: {},
+  },
+  title: "Power Access Overview",
+  imgSrc: "/images/gaza-powerplant.jpg",
+  kind: "image",
+  layerOverrides: [],
+};
+
+export const powerAccess = [gazaPowerPlant];
