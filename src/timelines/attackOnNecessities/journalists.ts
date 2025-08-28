9d1122b48c92700c558d1fb87041c730b4d883e3
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2025-08-27 23:16:59 -0400

    journalists killed

--- src/timelines/attackOnNecessities/journalists.ts
+++ src/timelines/attackOnNecessities/journalists.ts
@@ -1,3 +1,17 @@
-import type { Timeline } from "../timelines";
+import { GAZA_BBOX } from "../../constants/constants";
+import type { GazaEvent, Timeline } from "../timelines";
 
-export const journalists: Timeline = [];
+const attackOnJournalists: GazaEvent = {
+  fitBounds: {
+    bounds: GAZA_BBOX,
+    options: {},
+  },
+  imgSrc: "/images/journalists.png",
+  kind: "image-only",
+  layerOverrides: [
+    { id: "WHOLE_WORLD_FILL", paintOverrides: { "fill-opacity": 1 } },
+  ],
+  title: "Attack on Journalists (243 till now)",
+};
+
+export const journalists: Timeline = [attackOnJournalists];
