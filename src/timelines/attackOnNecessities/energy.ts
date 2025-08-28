742429f9479a7a8d1a3cfb9ba1c99784861107cf
Ahmad <1ahmadkamal1@gmail.com>
2025-08-27 23:54:50 -0400

    Added attack on Energy

--- src/timelines/attackOnNecessities/energy.ts
+++ src/timelines/attackOnNecessities/energy.ts
@@ -1,3 +1,83 @@
-import type { Timeline } from "../timelines";
+import { GAZA_BBOX } from "../../constants/constants";
+import { IMAGE_MAP } from "../../constants/images";
+import { GAZA_PPLANT_BOUNDS } from "../../constants/constants";
+import type { GazaEvent, Timeline } from "../timelines";
 
-export const energy: Timeline = [];
+const gazaPowerPlantDestroyed: GazaEvent = {
+    fitBounds: {
+      bounds: GAZA_PPLANT_BOUNDS,
+      options: {},
+    },
+    title: "Power Plant not Operational",
+    imgSrc: "/images/power-plant-fire.jpg",
+    kind: "image",
+    layerOverrides: [
+      {
+        id: "GAZA_POWER_SYMBOL",
+        paintOverrides: {
+          "icon-opacity": 1,
+          "icon-opacity-transition": { duration: 1000 },
+        },
+        "layoutOverrides": {
+          "icon-allow-overlap": true,
+        }
+      },
+      
+    ],
+  };
+  
+const meltingPlasticForFuel: GazaEvent = {
+    fitBounds: {
+    bounds: GAZA_BBOX,
+    options: {},
+    },
+    title: "Melting Plastic for Fuel",
+    imgSrc: "/images/energy-melting-plastic.jpg",
+    kind: "image-only",
+    layerOverrides: [
+    ],
+};
+
+const rubbleAsFuel: GazaEvent = {
+    fitBounds: {
+        bounds: GAZA_BBOX,
+        options: {},
+    },
+    title: "Using Rubble for Cooking",
+    imgSrc: "/images/energy-rubble-as-fuel.webp",
+    kind: "image-only",
+    layerOverrides: [
+    ],
+};
+
+const phoneChargingStations: GazaEvent = {
+    fitBounds: {
+        bounds: GAZA_BBOX,
+        options: {},
+    },
+    title: "Phone Charging Stations",
+    imgSrc: "/images/energy-phone-charging-stations.webp",
+    kind: "image-only",
+    layerOverrides: [
+    ],
+};
+
+const solarPanelDestroyed: GazaEvent = {
+    fitBounds: {
+        bounds: GAZA_BBOX,
+        options: {},
+    },
+    title: "Solar Panels Destroyed",
+    imgSrc: "/images/energy-solar-panel-destroyed.webp",
+    kind: "image-only",
+    layerOverrides: [
+    ],
+};
+
+export const energy: Timeline = [
+    gazaPowerPlantDestroyed,
+    meltingPlasticForFuel,
+    rubbleAsFuel,
+    phoneChargingStations,
+    solarPanelDestroyed,
+];
