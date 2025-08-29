033988ede7e8e4e56b4c0f3dce9f75d945dbebba
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2025-08-29 15:20:00 -0400

    fix

--- src/timelines/displacementTimeline/phases.5.ts
+++ src/timelines/displacementTimeline/phases.5.ts
@@ -34,49 +34,6 @@ const initialOrders: GazaEvent = {
   title: "Initial Orders",
 };
 
-const gazaFragmentationPlan: GazaEvent = {
-  kind: "basic",
-  fitBounds: {
-    bounds: GAZA_BBOX,
-    options: { padding: 40 },
-  },
-  layerOverrides: [
-    {
-      id: "GAZA_FRAGMENTATION_RASTER",
-      paintOverrides: { "raster-opacity": 1 },
-    },
-  ],
-  title: "Gaza Fragmentation Plan",
-};
-
-const moragIntro: GazaEvent = {
-  kind: "basic",
-  fitBounds: {
-    bounds: GAZA_BBOX,
-    options: {},
-  },
-  layerOverrides: [
-    { id: "MORAG_LINE", paintOverrides: { "line-opacity": 1 } },
-    { id: "MORAG_SYMBOL", paintOverrides: { "text-opacity": 1 } },
-  ],
-  title: "Introduction of the Morag Corridor",
-};
-
-const magenOzIntro: GazaEvent = {
-  kind: "basic",
-  fitBounds: {
-    bounds: GAZA_BBOX,
-    options: {},
-  },
-  layerOverrides: [
-    { id: "MORAG_LINE", paintOverrides: { "line-opacity": 1 } },
-    { id: "MORAG_SYMBOL", paintOverrides: { "text-opacity": 1 } },
-    { id: "MAGEN_OZ_LINE", paintOverrides: { "line-opacity": 1 } },
-    { id: "MAGEN_OZ_SYMBOL", paintOverrides: { "text-opacity": 1 } },
-  ],
-  title: "Introduction of the Magen Oz Corridor",
-};
-
 const gideonsChariot: GazaEvent = {
   kind: "image",
   imgSrc: "/images/gideons-chariot-girl-escapes-fire.jpg",
@@ -110,9 +67,6 @@ const ghfIntro: GazaEvent = {
 export const phaseFive: Timeline = [
   init,
   initialOrders,
-  gazaFragmentationPlan,
-  moragIntro,
-  magenOzIntro,
-  gideonsChariot,
   ghfIntro,
+  gideonsChariot,
 ];
--- src/timelines/displacementTimeline/phases.6.ts
+++ src/timelines/displacementTimeline/phases.6.ts
@@ -1,11 +1,5 @@
-import type { LngLatBoundsLike } from "mapbox-gl";
 import { GAZA_BBOX } from "../../constants/constants";
 import type { GazaEvent, Timeline } from "../timelines";
-
-const WEST_BANK_BBOX: LngLatBoundsLike = [
-  [34.85164950175996, 31.3020615747599],
-  [35.89104780349571, 32.598067520536446],
-];
 
 const init: GazaEvent = {
   kind: "image-only",
@@ -47,70 +41,10 @@ const smotrichQuote: GazaEvent = {
   title: "Smotrich Quote",
 };
 
-const westBankPlan: GazaEvent = {
-  kind: "basic",
-  fitBounds: {
-    bounds: WEST_BANK_BBOX,
-    options: { padding: 80 },
-  },
-  layerOverrides: [
-    {
-      id: "WEST_BANK_PLAN_RASTER",
-      paintOverrides: {
-        "raster-opacity": 0.9,
-      },
-    },
-  ],
-
-  title: "The Plan for the West Bank",
-};
-
-const e1Plan: GazaEvent = {
-  kind: "image",
-  imgSrc: "/images/e1-larger.jpg",
-  fitBounds: {
-    bounds: [
-      [35.21647846810884, 31.744750439847078],
-      [35.3697843906192, 31.845701490924586],
-    ],
-    options: { padding: 80 },
-  },
-  layerOverrides: [
-    {
-      id: "E1_PLAN_RASTER",
-      paintOverrides: {
-        "raster-opacity": 0.8,
-      },
-    },
-  ],
-
-  title: "Proposed E1 Settlement",
-};
-
-const gazaTakeover: GazaEvent = {
-  kind: "basic",
-  fitBounds: {
-    bounds: GAZA_BBOX,
-    options: { padding: 80 },
-  },
-  layerOverrides: [
-    {
-      id: "GAZA_TAKEOVER_RASTER",
-      paintOverrides: {
-        "raster-opacity": 0.8,
-      },
-    },
-  ],
-
-  title: "Plan for Gaza's Takeover",
-};
-
 const heathrowComparison: GazaEvent = {
   kind: "basic",
   fitBounds: {
     bounds: [
-      //   [-1.559897133489262, 51.06463390638773],
-      //   [0.396326493144797, 52.31886014963128],
       [-0.07344267363210126, 51.507832025123385],
       [-0.07344267363210126, 51.507832025123385],
     ],
@@ -128,11 +62,4 @@ const heathrowComparison: GazaEvent = {
   title: "Comparisons to London",
 };
 
-export const phaseSix: Timeline = [
-  init,
-  smotrichQuote,
-  westBankPlan,
-  e1Plan,
-  gazaTakeover,
-  heathrowComparison,
-];
+export const phaseSix: Timeline = [init, smotrichQuote, heathrowComparison];
