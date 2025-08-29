0aca77bf73e9d1c5d4d4fcec97d181105a873512
Amir Azizafshari <55924754+amir002001@users.noreply.github.com>
2025-08-28 21:31:39 -0400

    done water

--- src/constants/images.ts
+++ src/constants/images.ts
@@ -1,6 +1,8 @@
 const IMAGE_IDS = [
   "school",
   "red-chevron",
+  "broken-pipe",
+  "broken-factory",
   "tank",
   "hospital",
   "wwts",
@@ -37,6 +39,8 @@ export const MAPBOX_IMAGES: IImage[] = [
   { id: "bad-rocket", url: "/mapbox-icons/rocket.png" },
   { id: "hospital-destroyed", url: "/mapbox-icons/hospital-destroyed.png" },
   { id: "tank", url: "/mapbox-icons/tank.png" },
+  { id: "broken-pipe", url: "/mapbox-icons/broken-pipe.png" },
+  { id: "broken-factory", url: "/mapbox-icons/broken-factory.png" },
 ];
 
 export const IMAGE_MAP = new Map(IMAGE_IDS.map((id) => [id, id]));
